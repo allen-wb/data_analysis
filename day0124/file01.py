f1c85b890705b34f818853d30979214e08024ba0
wangbao <wangbao@kedacom.com>
2020-12-14 09:34:02 +0800

    jupyter

--- day0124/file01.py
+++ day0124/file01.py
@@ -5,3 +5,10 @@ b = np.array([4, 5, 6], ndmin=2, dtype=complex)
 print(a)
 print("-----")
 print(b)
+
+ar = np.array([1, 2, 3, 4, 5, 6, 7])
+print(ar)
+print(ar.shape)
+print(ar.size)
+print(ar.ndim)
+
